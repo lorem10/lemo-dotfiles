ef57b1856e557db03c32fbf5146955dcf3230f3e
lorem10 <momenei.alireza1382.15@gmail.com>
2022-08-07 09:10:11 +0000

    chore: update polybar config

---
+++ config/polybar/launch.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+killall -q polybar
+
+# Launch bar1 and bar2
+#echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+#polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
+#polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+
+#if you have tow manitor
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
+
+echo "Bars launched..."
